826c40785f2ee622070b9b780a3eab748909869a
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-10-12 17:19:53 +0200

    Fixed BMI calculator, only returned a hardcoded string before

--- calculateBmi.ts
+++ calculateBmi.ts
@@ -1,7 +1,14 @@
 type Message = String
 
-const calculateBmi = (height: number, weight: number) : Message => {
-    return 'Normal (healthy weight)'
+const calculateBmi = (height: number, weight: number) : string => {
+    const BMI = weight / Math.pow(height / 100, 2)
+
+    console.log(typeof BMI)
+
+    if(BMI <= 18.4) return 'Underweight'
+    if(BMI >= 18.5 && BMI <= 24.9) return 'Normal'
+    if(BMI >= 25.0 && BMI <= 29.9) return 'Overweight'
+    if(BMI >= 30.0) return 'Obese'
 }
 
-console.log(calculateBmi(180, 74))
+console.log(calculateBmi(180, 300))
