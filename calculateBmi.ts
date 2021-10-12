44b5dff3acf131db794f739bf0508aa74f86c6b0
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-10-12 22:21:36 +0200

    Added a way of validating input coming from the CLI + error handling

--- calculateBmi.ts
+++ calculateBmi.ts
@@ -1,14 +1,32 @@
-const height: number = Number(process.argv[2])
-const weight: number = Number(process.argv[3])
+interface bmiDetails {
+    value1: number,
+    value2: number
+}
+
+const checkArguments = (args: Array<string>) : bmiDetails => {
+    if(args.length > 4) throw new Error('Too many arguments passed in')
+    if(args.length < 4) throw new Error('Too few arguments passed in')
+
+    if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else {
+        throw new Error('Input should be all numbers!')
+    }
+}
+
+const {value1, value2} = checkArguments(process.argv)
 
 const calculateBmi = (height: number, weight: number) : string => {
     const BMI = weight / Math.pow(height / 100, 2)
 
-    if(BMI <= 18.4) return 'Underweight'
-    if(BMI >= 18.5 && BMI <= 24.9) return 'Normal'
-    if(BMI >= 25.0 && BMI <= 29.9) return 'Overweight'
-    if(BMI >= 30.0) return 'Obese'
+    if(BMI <= 18.4) return `${height} ${weight} Underweight`
+    if(BMI >= 18.5 && BMI <= 24.9) return `${height} ${weight} Normal`
+    if(BMI >= 25.0 && BMI <= 29.9) return `${height} ${weight} Overweight`
+    if(BMI >= 30.0) return `${height} ${weight} Obese`
 }
 
-console.log(calculateBmi(height, weight))
+console.log(calculateBmi(value1, value2))
 
