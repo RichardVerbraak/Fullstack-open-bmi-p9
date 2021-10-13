0d5f2520f409ccb5c96e26702b94a220f5f07f64
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-10-13 17:31:26 +0200

    Added additional comment

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -15,7 +15,7 @@ interface CalculationArgs {
 }
 
 const validateArgs = (args: Array<string>) : CalculationArgs => {
-    // Remove first 3 elements returned from process.argv
+    // Remove first 3 elements returned from process.argv -- first two being related to the directory and the last being the target hours
     const slicedArray = args.splice(0, 3)
     const target = Number(slicedArray[2])
 
