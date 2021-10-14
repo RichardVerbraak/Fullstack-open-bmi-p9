62bedac7eb926157e3846fa9ed9497007050b4e6
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-10-14 16:18:27 +0200

    Added a test for bmi endpoint

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 import express from 'express'
 const app = express()
 
-app.get('/ping', (_req, res) => {
-    res.send('ping')
+app.get('/hello', (_req, res) => {
+    res.send('Hello Full Stack!')
 })
 
-const PORT = 3003
+app.get('/bmi', (req, res) => {
+    const height = req.query.height
+    const weight = req.query.weight
+
+    res.status(200)
+    res.json({
+        weight,
+        height,
+        bmi: "Normal (Healthy weight)"
+    })
+})
+
+const PORT = 3002
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
